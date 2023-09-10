020f4e36722b2748bbb80ad1da73c1c1f824f5fe
Hacer Yelci <haceryelci@gmail.com>
2023-09-10 20:02:48 +0100

    updated modal and implemented submit action

--- modal.js
+++ modal.js
@@ -1,4 +1,7 @@
 const modal = document.getElementById("user-details-modal");
+const userForm = document.getElementById("user-details-form");
+const successText = document.getElementById("success-text");
+const submitBtn = document.getElementById("form-submit-btn");
 
 const downloadButton = document.getElementById("download-ebook-btn");
 const closeBtn = document.getElementById("modal-close-btn");
@@ -8,14 +11,31 @@ downloadButton.onclick = function () {
   modal.style.display = "block";
 };
 
+function handleModalClose() {
+  modal.style.display = "none";
+  successText.style.display = "none";
+  submitBtn.textContent = "Submit";
+  submitBtn.style.background = "#444bf7";
+}
+
 // Close modal
 closeBtn.onclick = function () {
-  modal.style.display = "none";
+  handleModalClose();
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    handleModalClose();
   }
 };
+
+// when user submits the form
+userForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Submitted the form");
+
+  successText.style.display = "block";
+  submitBtn.textContent = "Sent";
+  submitBtn.style.background = "#38c983";
+});
